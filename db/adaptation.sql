57a6b1a91401ffbe815e0b1280ba8fef50b8950c
Williams Gutierrez <ggiraldow@gmail.com>
2025-03-17 19:40:51 -0500

    add new table adaptation

--- db/adaptation.sql
+++ db/adaptation.sql
@@ -22,35 +22,52 @@ INSERT INTO `categoria_conocimiento` (`id`, `nombre`, `descripcion`) VALUES
 (4, 'Obligatorio complementario', 'Descipción obligatorio complementario');
 
 --
--- AUTO_INCREMENT for table `res_asignacion_resultados_de_aprendizaje`
+-- AUTO_INCREMENT for table `categoria_conocimiento`
 --
-ALTER TABLE `res_asignacion_resultados_de_aprendizaje`
+ALTER TABLE `categoria_conocimiento`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 
-
-
-
-
+--
+-- Table structure for table `asignatura`
+--
+CREATE TABLE `asignatura` (
+    `id` int NOT NULL,
+    `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+    `descripcion` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+    `categoria_id` int NOT NULL,
+	PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
 
 --
--- Indexes for table `res_asignacion_objetos_de_estudio`
+-- Dumping data for table `asignatura`
 --
-ALTER TABLE `res_asignacion_objetos_de_estudio`
-  ADD KEY `codigo_profesor` (`codigo_profesor`),
-  ADD KEY `codigo_objetos_de_estudio` (`codigo_objetos_de_estudio`),
-  ADD KEY `codigo_espacio` (`codigo_espacio`);
+INSERT INTO `asignatura` (`id`, `nombre`, `descripcion`, `categoria_id`) VALUES
+(1, 'Cálculo Diferencial', 'Descipción ciencias básicas', 1),
+(2, 'Cálculo Integral', 'Descipción ciencias básicas', 1),
+(3, 'Programación Web', 'Descipción ingeniería aplicada', 2),
+(4, 'Estructura de Datos', 'Descipción ingeniería aplicada', 2),
+(5, 'Base de Datos', 'Descipción básico profesional', 3),
+(6, 'Programación por Componentes', 'Descipción básico profesional', 3),
+(7, 'Producción y Comprensión de Textos I', 'Descipción obligatorio complementario', 4),
+(8, 'Ciencia, Tecnología y Sociedad', 'Descipción obligatorio complementario', 4);
+
+--
+-- AUTO_INCREMENT for table `asignatura`
+--
+ALTER TABLE `asignatura`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Indexes for table `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD KEY `categoria_id` (`categoria_id`);
 
 --
 -- Constraints for table `res_asignacion_objetos_de_estudio`
 --
-ALTER TABLE `res_asignacion_objetos_de_estudio`
-  ADD CONSTRAINT `res_asignacion_objetos_de_estudio_ibfk_1` FOREIGN KEY (`codigo_profesor`) REFERENCES `res_profesor` (`codigo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `res_asignacion_objetos_de_estudio_ibfk_2` FOREIGN KEY (`codigo_objetos_de_estudio`) REFERENCES `res_objetos_de_estudio` (`codigo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `res_asignacion_objetos_de_estudio_ibfk_3` FOREIGN KEY (`codigo_espacio`) REFERENCES `res_espacio` (`codigo`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `asignatura`
+  ADD CONSTRAINT `asignatura` FOREIGN KEY (`categoria_id`) REFERENCES `categoria_conocimiento` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- AUTO_INCREMENT for table `res_asignacion_resultados_de_aprendizaje`
---
-ALTER TABLE `res_asignacion_resultados_de_aprendizaje`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=348;
 
--- db/script.sql
+++ db/script.sql
@@ -1,32 +1,25 @@
-DROP DATABASE IF EXISTS proyecto_grado_db;
-CREATE DATABASE proyecto_grado_db;
-USE proyecto_grado_db;
-
-DROP TABLE IF EXISTS categoria_conocimiento;
-
-CREATE TABLE categoria (
+CREATE TABLE categoria_conocimiento (
 	id INT AUTO_INCREMENT,
 	nombre VARCHAR(50) NOT NULL UNIQUE,
 	descripcion VARCHAR(200) NOT NULL,
-	PRIMARY KEY(id) 
-)
+	PRIMARY KEY(id)
+);
 
-INSERT INTO categoria_conocimiento(nombre, descripcion)
+INSERT INTO categoria_conocimiento (nombre, descripcion)
 VALUES
 ('Ciencias básicas', 'Descipción ciencias básicas'),
 ('Ingeniería aplicada', 'Descipción ingeniería aplicada'),
 ('Básico profesional', 'Descipción básico profesional'),
 ('Obligatorio complementario', 'Descipción obligatorio complementario');
 
-
 CREATE TABLE asignatura (
 	id INT AUTO_INCREMENT,
 	nombre VARCHAR(50) NOT NULL UNIQUE,
 	descripcion VARCHAR(200) NOT NULL,
 	categoria_id INT NOT NULL,
-	FOREIGN KEY(categoria_id) REFERENCES categoria(id),
+	FOREIGN KEY(categoria_id) REFERENCES categoria_conocimiento(id),
 	PRIMARY KEY(id)
-)
+);
 
 INSERT INTO asignatura(nombre, descripcion, categoria_id)
 VALUES
@@ -39,31 +32,25 @@ VALUES
 ('Producción y Comprensión de Textos I', 'Descipción obligatorio complementario', 4),
 ('Ciencia, Tecnología y Sociedad', 'Descipción obligatorio complementario', 4);
 
-CREATE TABLE herramienta (
+CREATE TABLE resultado_concreto_asignatura (
 	id INT AUTO_INCREMENT,
-	nombre VARCHAR(50) NOT NULL UNIQUE,
+	nombre VARCHAR(50) NOT NULL,
 	descripcion VARCHAR(200) NOT NULL,
+	asignatura_id INT NOT NULL,
+	FOREIGN KEY(asignatura_id) REFERENCES asignatura(id),
 	PRIMARY KEY(id)
 );
 
-INSERT INTO herramienta(nombre, descripcion)
+INSERT INTO resultado_concreto_asignatura (nombre, descripcion, asignatura_id)
 VALUES
-('MySQL', 'Motor de bases de datos'),
-('Eclipse', 'Entorno de desarrollo'),
-('Programación orientada a objetos, un enfoque práctico', 'Texto guía'),
-('MySQL Workbench', 'Entorno gráfico para bases de datos');
+('nombre', 'El estudiante debe estar en la capacidad de realizar derivadas de diversa índole', 1),
+('nombre', 'El estudiante debe estar en la capacidad de realizar integrales de diversa índole', 2),
+('nombre', 'El estudiante debe estar en la capacidad de desarrollar aplicaciones web', 3),
+('nombre', 'El estudiante debe reconocer las distintas estructuras de datos en C++', 4),
+('nombre', 'El estudiante debe estar en la capacidad de reconocer conceptos básicos de Bases de Datos', 5),
+('nombre', 'El estudiante debe estar en la capacidad de desarrollar aplicaciones distribuidas', 6),
+('nombre', 'El estudiante debe estar en la capacidad de realizar ensayos básicos', 7),
+('nombre', 'El estudiante debe estar en la capacidad de realizar informes científicos básicos', 8);
 
-CREATE TABLE asignatura_herramienta_int (
-	asignatura_id INT NOT NULL,
-	herramienta_id INT NOT NULL,
-	FOREIGN KEY(asignatura_id) REFERENCES asignatura(id),
-	FOREIGN KEY(herramienta_id) REFERENCES herramienta(id),
-	PRIMARY KEY(asignatura_id, herramienta_id)
-);
 
-INSERT INTO asignatura_herramienta_int(asignatura_id, herramienta_id)
-VALUES
-(1, 1),
-(2, 2),
-(1, 3),
-(3, 4);
+
