d89840f2ccde829caf678850ecc75e6a5f70e98d
Diego Hernando Rodríguez Góngora <diego.rodriguez@insoftar.com>
2025-07-07 19:27:42 -0500

    Prototipo inicial de Login

--- Controllers/Category.php
+++ Controllers/Category.php
@@ -1,18 +1,17 @@
 <?php
+    session_start();
     class Category extends Controllers{
         public function __construct(){
             parent::__construct();
         }
 
         public function Category(int $codeLR){
-
-                $data['page_tag'] = $this->getCategoryTitle($codeLR);
-                $data['page_title'] = $this->getCategoryTitle($codeLR);
-                $data['page_functions_js'] = "functions_categories.js";
-                echo("<script>console.log('PHP: test log category');</script>");
-                $this->views->getView($this,"Category",$data);
-
-
+            $data['page_tag'] = $this->getCategoryTitle($codeLR);
+            $data['page_title'] = $this->getCategoryTitle($codeLR);
+            $data['page_functions_js'] = "functions_categories.js";
+            echo("<script>console.log('PHP: test log category');</script>");
+            echo $_SESSION['session'];
+            $this->views->getView($this,"Category",$data);
         }
 
         public function getCategoryTitle(int $codeLR){
@@ -28,14 +27,24 @@
     
         public function getSubjectsByCategory(int $codeLR){
             $arrData = $this->model->getSubjectsByCategory($codeLR);
-            for($i=0; $i<count($arrData); $i++){
-                $arrData[$i]['acciones'] = '<div class="text-center">
-                <button class="btn btn-outline-secondary btn-sm" id="btnEditLR" onclick="getSubjectInfo(this)" title="Editar" lr="'.$arrData[$i]['id'].'"><i class="fas fa-pencil-alt"></i></button>
-                <button class="btn btn-outline-danger btn-sm" id="btnDeleteLR" onclick="deleteLearningResult(this) "title="Eliminar" lr="'.$arrData[$i]['id'].'"><i class="far fa-trash-alt"></i></button>
-                </div>';
-            };
-            echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
-            die();
+            if (isset($_SESSION['session'])) {
+                for($i=0; $i<count($arrData); $i++){
+                    $arrData[$i]['acciones'] = '<div class="text-center">
+                    <button class="btn btn-outline-secondary btn-sm" id="btnEditLR" onclick="getSubjectInfo(this)" title="Editar" lr="'.$arrData[$i]['id'].'"><i class="fas fa-pencil-alt"></i></button>
+                    <button class="btn btn-outline-danger btn-sm" id="btnDeleteLR" onclick="deleteLearningResult(this) "title="Eliminar" lr="'.$arrData[$i]['id'].'"><i class="far fa-trash-alt"></i></button>
+                    </div>';
+                };   
+                echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
+                die();         
+            }else {
+                for($i=0; $i<count($arrData); $i++){
+                    $arrData[$i]['acciones'] = '<div class="text-center">
+                    <button class="btn btn-outline-secondary btn-sm" id="btnEditLR" onclick="getSubjectInfo(this)" title="Editar" lr="'.$arrData[$i]['id'].'"><i class="fas fa-pencil-alt"></i></button>
+                    </div>';
+                };
+                echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
+                die();
+            }
         }
     }
 ?>
--- Controllers/Login.php
+++ Controllers/Login.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     class Login extends Controllers{
 
         public function __construct(){
@@ -11,5 +12,28 @@
             $data['page_functions_js'] = "functions_login.js";
             $this->views->getView($this,"Login",$data);
         }
-    }
+
+        public function validateSession(){
+            $strUser = strtolower(strClean($_POST['txtUser']));
+            $strPassword = strClean($_POST['txtPassword']);
+            
+            $requestUser = $this->model->loginUser($strUser, $strPassword);
+            if(!empty($requestUser)){
+                $data['page_tag'] = "Modificar Resultados de Aprendizaje";
+                $data['page_title'] = "Modificación Resultados de Aprendizaje";
+                $data['page_functions_js'] = "functions_edit_lr.js";
+                $data['user'] = $strUser;
+                $data['pass'] = $strPassword;
+                $_SESSION['session'] =   $strUser;//hash("SHA256", $strPassword);            
+                echo "<script>
+                    window.location= '".baseUrl()."'
+                </script>";
+                exit();
+            } else {
+                echo "<script> alert('Credenciales incorrectas');
+                    window.location= '".baseUrl()."Login'
+                </script>";
+            }
+        }
+    }    
 ?>
